d437f53f816f26289146624cf171e6392c08cffc
CarlosPantin~ <carlospantin@live.com.mx>
2023-09-26 13:13:43 +0300

    Changes to be committed: modified:   package-lock.json modified:   package.json modified:   src/App.css modified:   src/App.js new file:   src/components/Board.css new file:   src/components/Board.js new file:   src/components/Square.css new file:   src/components/Square.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Board from './components/Board';
+import Confetti from 'react-confetti'; 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+      currentPlayer: 'X',
+      winner: null,
+    };
+  }
+
+  handleClick(index) {
+    if (this.state.squares[index] || this.state.winner) {
+      return;
+    }
+
+    const squares = [...this.state.squares];
+    squares[index] = this.state.currentPlayer;
+    const winner = this.calculateWinner(squares);
+    const currentPlayer = this.state.currentPlayer === 'X' ? 'O' : 'X';
+
+    this.setState({
+      squares,
+      currentPlayer,
+      winner,
+    });
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const [a, b, c] of lines) {
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  }
+
+  resetGame() {
+    this.setState({
+      squares: Array(9).fill(null),
+      currentPlayer: 'X',
+      winner: null,
+    });
+  }
+
+  render() {
+    const status = this.state.winner
+      ? `Winner: ${this.state.winner}`
+      : `Next player: ${this.state.currentPlayer}`;
+
+      return (
+        <div className="app">
+          <h1>Tic-Tac-Toe</h1>
+          {this.state.winner && <Confetti />}
+          <Board
+            squares={this.state.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+          <div className="game-status">{status}</div>
+          {this.state.winner && (
+            <button onClick={() => this.resetGame()} className="button">
+              Play Again
+            </button>
+          )}
+        </div>
+      );
+      
+  }
 }
 
 export default App;
